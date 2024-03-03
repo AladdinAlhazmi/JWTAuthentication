497e67192c2fb895b2df45cb7fc96120793cb081
Aladdin Alhazmi <53558716+AladdinAlhazmi@users.noreply.github.com>
2024-03-03 12:04:54 +0300

    add Employee Mapper class

---
+++ Mapper/EmployeeMapper.cs
@@ -0,0 +1,13 @@
+﻿using JWTAuthentication.DTOs.Employee;
+using JWTAuthentication.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace JWTAuthentication.Mapper
+{
+    [Mapper]
+    public partial class EmployeeMapper
+    {
+        public partial EmployeeDTO EmployeeToEmployeeDto(Employee employee);
+        public partial Employee EmployeeDtoToEmployee(EmployeeDTO employeeDTO);
+    }
+}
